1c1b7910bd1e786b4223d300d6bdfda263757b0c
Leandro Almeida <almeida.leandro.a@gmail.com>
2024-12-07 19:14:42 +0000

    Add springdoc-openapi documentation

--- src/main/java/com/challenge/phonemanagement/configuration/BeanConfiguration.java
+++ src/main/java/com/challenge/phonemanagement/configuration/BeanConfiguration.java
@@ -2,7 +2,10 @@ package com.challenge.phonemanagement.configuration;
 
 import com.challenge.phonemanagement.configuration.properties.AbstractApiProperties;
 import com.challenge.phonemanagement.interceptor.CustomClientHttpRequestInterceptor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -25,5 +28,15 @@ public class BeanConfiguration {
                 .baseUrl(apiProperties.getBaseUrl())
                 .requestInterceptor(new CustomClientHttpRequestInterceptor())
                 .build();
+    }
+
+    @Bean
+    public OpenAPI phoneManagementOpenAPI(
+            @Value("${application-description}") String appDescription,
+            @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info().title("Phone Management API")
+                        .description(appDescription)
+                        .version(appVersion));
     }
 }
--- src/main/java/com/challenge/phonemanagement/controller/PhoneController.java
+++ src/main/java/com/challenge/phonemanagement/controller/PhoneController.java
@@ -3,6 +3,10 @@ package com.challenge.phonemanagement.controller;
 import com.challenge.phonemanagement.model.dto.phone.CreatePhoneDto;
 import com.challenge.phonemanagement.model.dto.phone.PhoneDto;
 import com.challenge.phonemanagement.service.phone.PhoneService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@Tag(name = "Phone")
 @RestController
 @RequestMapping("/api/v1/phones")
 public class PhoneController {
@@ -20,6 +25,10 @@ public class PhoneController {
         this.phoneService = phoneService;
     }
 
+    @Operation(summary = "Create phone record", responses = {
+            @ApiResponse(description = "Successful operation", responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "400", description = "The number is invalid", content = @Content),
+            @ApiResponse(responseCode = "409", description = "The phone number already exists", content = @Content)})
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody CreatePhoneDto phoneDto) {
         PhoneDto createdRecord = phoneService.create(phoneDto);
@@ -33,12 +42,16 @@ public class PhoneController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(summary = "Get All phone records")
     @GetMapping
     public ResponseEntity<List<PhoneDto>> getAll() {
         List<PhoneDto> phones = phoneService.getAll();
         return ResponseEntity.ok(phones);
     }
 
+    @Operation(summary = "Get a phone records by ID", responses = {
+            @ApiResponse(description = "Successful operation", responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Phone number not found for the given Id", content = @Content)})
     @GetMapping("/{id}")
     public ResponseEntity<PhoneDto> getById(@PathVariable Long id) {
         PhoneDto phoneDto = phoneService.getById(id);
