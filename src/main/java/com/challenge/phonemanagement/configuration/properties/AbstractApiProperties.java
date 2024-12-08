aba39624a62f7fdec17001e64ed021815d390317
Leandro Almeida <almeida.leandro.a@gmail.com>
2024-12-08 00:29:24 +0000

    Add missing validation on properties

--- src/main/java/com/challenge/phonemanagement/configuration/properties/AbstractApiProperties.java
+++ src/main/java/com/challenge/phonemanagement/configuration/properties/AbstractApiProperties.java
@@ -1,12 +1,19 @@
 package com.challenge.phonemanagement.configuration.properties;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "third-party.api.abstract")
 public class AbstractApiProperties {
+
+    @NotBlank
     private String baseUrl;
+
+    @NotBlank
     private String apiKey;
 
     public String getBaseUrl() {
